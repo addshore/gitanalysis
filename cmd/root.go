6b4ccd26ef2ade534bca474411277a84055b0e6b
addshore <addshorewiki@gmail.com>
2021-01-16 19:01:55 +0000

    Start using blame
    but god its slow...

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,10 @@
 package cmd
 
 import (
+	"io/ioutil"
+	"encoding/json"
+	"path/filepath"
+	"regexp"
 	"strings"
 	"os/user"
 	"log"
@@ -27,18 +31,24 @@ func Execute(appVersion string, appSourceDate string) {
 	}
 }
 
-type GitRepo string
-
-func (r GitRepo) Url() string {
-	return string(r)
+type Config struct {
+	Repos []Repo
 }
 
-func (r GitRepo) DirName() string {
-	s := string(r)
+type Repo struct {
+	Url string
+}
+
+func (r Repo) DirName() string {
+	s := string(r.Url)
 	s = strings.Replace(s,"://","_", -1)
 	s = strings.Replace(s,".git","", -1)
 	s = strings.Replace(s,"/","_", -1)
 	return s
+}
+
+func (r Repo) DirNameInDir( inDir string ) string {
+	return inDir + "/" + r.DirName()
 }
 
 var rootCmd = &cobra.Command{
@@ -57,9 +67,22 @@ var rootCmd = &cobra.Command{
 		// 	os.Exit(0)
 		// }
 
-		// TODO take repos as arguments or something
-		repos := [1]GitRepo{GitRepo("https://github.com/wikimedia/Wikibase.git")}
-		fmt.Printf("Analysing Repos: %s\n",repos)
+		// TODO read this from an argument or something?
+		configJson, err := ioutil.ReadFile("input.json")
+		if err != nil {
+			panic(err)
+		}
+		var config Config
+		err = json.Unmarshal(configJson, &config)
+		if err != nil { // don't forget handle errors
+			log.Fatal(err)
+		}
+
+		fmt.Printf(config.Repos[0].Url + "\n")
+
+		var repos = config.Repos
+
+		fmt.Printf("Analysing %s Repos\n",string(len(repos)))
 
 		user, err := user.Current()
 		if err != nil {
@@ -85,32 +108,85 @@ var rootCmd = &cobra.Command{
 
 		// Get or update the repos..
 		for i := 0; i < len(repos); i++ { 
+			// Clone or update the repo
 			repo := repos[i]
-			repoPath := workDir + "/" + repo.DirName()
-
-			gitRepo, err := git.PlainOpen(repoPath);
+			Repo, err := git.PlainOpen(repo.DirNameInDir(gitDir));
 			if(err != nil) {
-				fmt.Printf("Cloning: %s\n",repo.Url())
+				fmt.Printf("Cloning: %s\n",repo.Url)
 				// TODO check error
-				gitRepo, err = git.PlainClone(repoPath, false, &git.CloneOptions{
-					URL:      repo.Url(),
+				Repo, err = git.PlainClone(repo.DirNameInDir(gitDir), false, &git.CloneOptions{
+					URL:      repo.Url,
 					Progress: os.Stdout,
 				})
 			} else {
-				fmt.Printf("Repo already on disk, updating: %s\n",repo.Url())
+				fmt.Printf("Repo already on disk, updating: %s\n",repo.Url)
 				// TODO check error
-				gitWorkTree, _ := gitRepo.Worktree()
+				gitWorkTree, _ := Repo.Worktree()
 				// TODO check error
 				gitWorkTree.Pull(&git.PullOptions{RemoteName: "origin"})
 			}
-
 			if err != nil {
 				log.Fatal(err)
 			}
+
+			// Find all the files we care about (with some excluded)
+			pathIgnoreRegex, e := regexp.Compile("^.+/\\.git/.+$")
+			if e != nil {
+				log.Fatal(e)
+			}
+			e = filepath.Walk(repo.DirNameInDir(gitDir), func(path string, info os.FileInfo, err error ) error {
+				if info.IsDir() || err != nil {
+					return nil
+				}
+				// Ignore the .git directory
+				if pathIgnoreRegex.MatchString(path) {
+					//ignored
+				} else {
+					// Run a blame on the files that we are looking at
+					fileInRepo := strings.Replace(path,repo.DirNameInDir(gitDir) + "/","", -1)
+					head, _ := Repo.Head()
+					commit, _ := Repo.CommitObject(head.Hash())
+					// TODO do something with blame...
+					/*blameOut, err := */git.Blame(commit,fileInRepo)
+					if err != nil {
+						log.Fatal(err)
+					}
+				}
+				return nil
+			})
+			if e != nil {
+				log.Fatal(e)
+			}
+
+
 		}
 
-		// TODO ignore some files like tests?
-		// TODO run git blame on every file..?
+		/**
+		type BlameResult struct {
+	// Path is the path of the File that we're blaming.
+	Path string
+	// Rev (Revision) is the hash of the specified Commit used to generate this result.
+	Rev plumbing.Hash
+	// Lines contains every line with its authorship.
+	Lines []*Line
+}
+**/
+
+
+/**
+// Line values represent the contents and author of a line in BlamedResult values.
+type Line struct {
+	// Author is the email address of the last author that modified the line.
+	Author string
+	// Text is the original text of the line.
+	Text string
+	// Date is when the original text of the line was introduced
+	Date time.Time
+	// Hash is the commit hash that introduced the original line
+	Hash plumbing.Hash
+}
+**/ 
+
 		// TODO use regexes to separate files into "projects" or "areas"?
 		// TODO join the blames together in these areas to see the % that is written by who?
 		// TODO associate people with teams that currently work on it?
